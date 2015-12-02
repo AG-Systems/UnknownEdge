8c00d1644b1f998e2cc5e96b04b964571384042c
Auriga <AG-Systems@users.noreply.github.com>
2015-12-01 22:27:04 -0800

    Update alg.py

--- alg.py
+++ alg.py
@@ -4,11 +4,29 @@ import newspaper
 import urllib
 from newspaper import Article
 import nltk
+import time
 
+cnn_paper = newspaper.build('http://cnn.com')
 
-page = urllib.urlopen("http://xauriga.com/tech.html").read()
-summary = "hello my name is "
-newsummary = ''.join(('<p> ',summary, '</p>'))
-text = re.sub('<p>.*?</p>', newsummary , page)
-
-print text
+while(0<1):
+    for article in cnn_paper.articles:
+        print(article.url)
+    for category in cnn_paper.category_urls():
+        print(category)
+    time.sleep(60)
+    article = cnn_paper.articles[0]
+    article.download()
+    time.sleep(10)
+    article.parse()
+    time.sleep(10)
+    article.nlp()
+    time.sleep(10)
+    summary = article.summary
+    page = urllib.urlopen("http://xauriga.com/tech.html").read()
+    text_file = open("tech.html", "w")
+    newsummary = ''.join(('<p> ',summary, '</p>'))
+    text = re.sub('<p>.*?</p>', newsummary , page)
+    print text
+    text_file.write(text)
+    text_file.close()
+    time.sleep(600)
